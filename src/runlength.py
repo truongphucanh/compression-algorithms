977d3a9fa31e906ec42e82069fe62c994aa78f26
Truong Phuc Anh <truongphucanh.main@gmail.com>
2018-01-04 15:52:12 +0700

    feat: Clean up code

--- src/runlength.py
+++ src/runlength.py
@@ -1,22 +1,19 @@
-"""
-Runlength encoding module
-"""
+""" Runlength encoding module. """
 
 from itertools import groupby
 import pickle
 
 def encode(document):
-    """
-    Encode a document (document).
+    """ Encode a document (document).
 
     Parameters
     ----------
-    document : list of symbols (can be string)
+    document : string.
         Original document.
 
     Returns
     -------
-    encoded: list
+    encoded: list<int, char>.
         Encoded document as a list of pair <length of run, symbol>.
 
     """
@@ -24,13 +21,12 @@ def encode(document):
     return encoded
 
 def decode(lst):
-    """
-    Encode a document (document).
+    """ Encode a document (document).
 
     Parameters
     ----------
-    lst : list
-        Encoded document.
+    lst : list<int, char>
+        Encoded document as a list of pair <length of run, symbol>.
 
     Returns
     -------
@@ -41,6 +37,16 @@ def decode(lst):
     origin = ''.join(symbol * n_times for n_times, symbol in lst)
     return origin
 
+def test():
+    """ Just for testing. """
+    origin = str('WWWWWW3333WWBWWWW55555W62WWBBBWWWWW0-1W-1WWWW-1-1-1-1WWWWWWWWWWBWWWWWWWWWWWWWW')
+    print 'Origin doc:\t {}'.format(origin)
+    encoded = encode(origin)
+    print 'Encoded doc:\t {}'.format(encoded)
+    decoded = decode(encoded)
+    print 'Decoded doc:\t {}'.format(decoded)
+    assert decoded == origin
+
 def encode_file(in_file, out_file, ratio_file=None):
     """ Encode a document file.
 
@@ -50,10 +56,8 @@ def encode_file(in_file, out_file, ratio_file=None):
     out_file : Output file (encoded file).
     ratio_file : Compression ratio file.
     """
-    document = ''
-    file_reader = open(in_file, 'r')
-    document = file_reader.read()
-    file_reader.close()
+    with open(in_file, 'r') as file_reader:
+        document = file_reader.read()
 
     encoded_list = [(len(list(run)), symbol) for symbol, run in groupby(document)]
     with open(out_file, 'wb') as file_writer:
@@ -62,9 +66,8 @@ def encode_file(in_file, out_file, ratio_file=None):
     if ratio_file != None:
         encoded_str = ''.join(str(run_length) + str(symbol) for run_length, symbol in encoded_list)
         ratio = len(document) * 1.0 / len(encoded_str)
-        file_writer = open(ratio_file, 'a')
-        file_writer.write('{},{},{},{}\n'.format(in_file, len(document), len(encoded_str), ratio))
-        file_writer.close()
+        with open(ratio_file, 'a') as file_writer:
+            file_writer.write('{},{},{},{}\n'.format(in_file, len(document), len(encoded_str), ratio))
 
 def decode_file(in_file, out_file):
     """ Encode a document file.
@@ -85,16 +88,6 @@ def decode_file(in_file, out_file):
     file_writer.write(decoded)
     file_writer.close()
 
-def test():
-    """ Just for testing. """
-    origin = str('WWWWWW3333WWBWWWW55555W62WWBBBWWWWW0-1W-1WWWW-1-1-1-1WWWWWWWWWWBWWWWWWWWWWWWWW')
-    print 'Origin doc:\t {}'.format(origin)
-    encoded = encode(origin)
-    print 'Encoded doc:\t {}'.format(encoded)
-    decoded = decode(encoded)
-    print 'Decoded doc:\t {}'.format(decoded)
-    assert decoded == origin
-
 def validate(origin_file, decoded_file):
     """Validate a decoded file.
 
@@ -102,13 +95,12 @@ def validate(origin_file, decoded_file):
     -------
     Whether decoded file has the same content with the original file or not.
     """
-    file_reader = open(origin_file, 'r')
-    origin_doc = file_reader.read()
-    file_reader.close()
 
-    file_reader = open(decoded_file, 'r')
-    decoded_doc = file_reader.read()
-    file_reader.close()
+    with open(origin_file, 'r') as file_reader:
+        origin_doc = file_reader.read()
+
+    with open(origin_file, 'r') as file_reader:
+        decoded_doc = file_reader.read()
 
     return origin_doc == decoded_doc
 
